dfb4dec311f3375484daf12b1dd10b69307dbdaf
ermlikeyeah <si@studioofthings>
2016-03-21 15:42:46 +0000

    version 0-2 with correct add up

--- app/routes.js
+++ app/routes.js
@@ -45,8 +45,77 @@ router.get('/examples/over-18', function (req, res) {
 // **** My routes ****
 //
 
+//
+// v0.2
+//
 
-// Example form result page, replay the data from previous page
+router.get('/v0_2/page2', function (req, res) {
+
+  res.render('v0_2/page2', {
+    'withdrawal' : req.query.withdrawal
+  });
+
+});
+
+
+router.get('/v0_2/page3', function (req, res) {
+
+  res.render('v0_2/page3', {
+    'withdrawal' : req.query.withdrawal,
+    'year1' : req.query.year1,
+    'year2' : req.query.year2,
+    'year3' : req.query.year3,
+    'year4' : req.query.year4,
+    'year5' : req.query.year5,
+    'year6' : req.query.year6,
+    'year7' : req.query.year7,
+    'year8_1' : req.query.year8_1,
+    'year8_left1' : req.query.year8_left1,
+    'year8_2' : req.query.year8_2,
+    'year8_left2' : req.query.year8_left2
+  });
+
+});
+
+router.get('/v0_2/page4', function (req, res) {
+
+  res.render('v0_2/page4', {
+    'withdrawal' : req.query.withdrawal,
+    'year1' : req.query.year1,
+    'year2' : req.query.year2,
+    'year3' : req.query.year3,
+    'year4' : req.query.year4,
+    'year5' : req.query.year5,
+    'year6' : req.query.year6,
+    'year7' : req.query.year7,
+    'year8_1' : req.query.year8_1,
+    'year8_left1' : req.query.year8_left1,
+    'year8_2' : req.query.year8_2,
+    'year8_left2' : req.query.year8_left2,
+    'year8_1_total' : req.query.year8_1 - req.query.year8_left1,
+    'year8_2_total' : req.query.year8_2 - req.query.year8_left2,
+    'year9' : req.query.year9,
+    'year10' : req.query.year10,
+    'years_total' : parseInt(req.query.year1)
+                  + parseInt(req.query.year2)
+                  + parseInt(req.query.year3)
+                  + parseInt(req.query.year4)
+                  + parseInt(req.query.year5)
+                  + parseInt(req.query.year6)
+                  + parseInt(req.query.year7)
+                  + (parseInt(req.query.year8_1) - parseInt(req.query.year8_left1))
+                  + (parseInt(req.query.year8_2) - parseInt(req.query.year8_left2))
+                  + parseInt(req.query.year9)
+                  + parseInt(req.query.year10)
+
+  });
+
+});
+
+
+//
+// v0.1
+//
 
 router.get('/v0_1/page2', function (req, res) {
 
